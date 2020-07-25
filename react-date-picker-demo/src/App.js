8f1bf09d524434f75fb1af8df59a6c27a8f12a87
sundaramhpe <sunram_c@hotmail.com>
2020-07-25 06:59:19 +0530

    datepicker completed

--- react-date-picker-demo/src/App.js
+++ react-date-picker-demo/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker'
 import logo from './logo.svg';
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css'
 
 function App() {
+  const [selectDate, setSelectDate] = useState(null)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DatePicker
+        selected={selectDate}
+        onChange={date => setSelectDate(date)}
+        dateFormat='dd/MM/yy'
+        // minDate={new Date()}
+        //maxDate={new Date()}
+        //filterDate={date=> date.getDay()==! 6 && date.getDay()==! 0}
+        isClearable
+        showYearDropdown
+        scrollableYearDropdown
+      />
     </div>
   );
 }
